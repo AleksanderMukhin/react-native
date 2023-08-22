bbe9ab9f25650a78153141b800c19ced1c048083
AleksanderMukhin <mukhin.aleksander.ua@gmail.com>
2023-08-22 09:54:58 +0200

    first

--- App.js
+++ App.js
@@ -1,20 +1,41 @@
+
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Switch, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text}>Hello, react native!</Text>
       <StatusBar style="auto" />
+      <Switch />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+ container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+     alignItems: 'center',
     justifyContent: 'center',
-  },
+   ...Platform.select({
+      ios: {
+       backgroundColor: '#fdc'
+    },
+     android: {
+      backgroundColor: '#fdc'
+    },
+  }),
+ },
 });
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fdc',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+//   text: {
+//     fontSize: 40
+//   }
+// });
